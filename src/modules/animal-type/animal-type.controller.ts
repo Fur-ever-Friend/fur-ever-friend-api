f701edb8ba0fb252288adf4853e7a9f212505163
Prataad <pondaeroz008y@gmail.com>
2024-10-20 22:28:23 +0700

    feat: add crud pet, animal type, breed

--- src/modules/animal-type/animal-type.controller.ts
+++ src/modules/animal-type/animal-type.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { AnimalTypeService } from './animal-type.service';
+import { AnimalTypeDto } from './dto/create-animal-type.dto';
 
 @Controller('animal-types')
 export class AnimalTypeController {
@@ -13,5 +14,15 @@ export class AnimalTypeController {
     @Get(":animalTypeId")
     async getAnimalTypeById(@Param("animalTypeId") id: string) {
         return this.animalTypeService.getAnimalTypeById(id);
+    }
+
+    @Post()
+    async addAnimalType(@Body() { name }: AnimalTypeDto) {
+        return this.animalTypeService.addAnimalType(name);
+    }
+
+    @Put(":id")
+    async updateAnimalType(@Param("id") id: string, @Body() { name }: AnimalTypeDto) {
+        return this.animalTypeService.updateAnimalType(id, name);
     }
 }
