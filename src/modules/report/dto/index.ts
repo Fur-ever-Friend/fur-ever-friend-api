ccaba5e52595c392ba91fa15b6e3132933ded2fd
Prataad <pondaeroz008y@gmail.com>
2024-10-24 01:31:50 +0700

    feat: crud report fully features with query

---
+++ src/modules/report/dto/index.ts
@@ -0,0 +1,2 @@
+export * from './report-query-param.dto';
+export * from './create-report.dto';
--- src/modules/report/report.service.ts
+++ src/modules/report/report.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException, UseGuards } from '@nestjs/common';
-import { CreateReportDto } from './dto/create-report.dto';
-import { PrismaService } from '../prisma/prisma.service';
 import { Prisma, Role } from '@prisma/client';
+
+import { PrismaService } from '../prisma/prisma.service';
 import { UserService } from '../user/user.service';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
+import { CreateReportDto, ReportQueryDto } from './dto';
 
 @Injectable()
 export class ReportService {
@@ -13,12 +14,13 @@ export class ReportService {
     private readonly prismaService: PrismaService,
     private readonly userService: UserService,
   ) { }
+
   async create(createReportDto: CreateReportDto, reportImages: string[]) {
     const reporterId = createReportDto.reporterId;
     const reportedId = createReportDto.reportedId;
 
-    const reporter = this.userService.getUserById(reporterId);
-    const reported = this.userService.getUserById(reportedId);
+    const reporter = this.userService.getUserByIdWithoutCredential(reporterId);
+    const reported = this.userService.getUserByIdWithoutCredential(reportedId);
     if (!reporter || !reported) {
       throw new NotFoundException('Reporter or reported user not found');
     }
@@ -52,6 +54,7 @@ export class ReportService {
             avatar: true,
           }
         },
+        createdAt: true,
       }
     });
 
@@ -60,13 +63,23 @@ export class ReportService {
 
   @Roles(Role.ADMIN)
   @UseGuards(JwtAuthGuard, RolesGuard)
-  findAll() {
-    return this.prismaService.report.findMany({
+  async findAll(reportQueryDto: ReportQueryDto) {
+    const { type, startDate, endDate, page = 1, limit = 10 } = reportQueryDto;
+    const where: Prisma.ReportWhereInput = {};
+    if (type) where.type = type;
+    if (startDate && endDate) where.createdAt = { gte: startDate, lte: endDate };
+    console.log(where);
+    console.log(reportQueryDto);
+    const reports = await this.prismaService.report.findMany({
+      where,
+      take: limit,
+      skip: (page - 1) * limit,
       select: {
         id: true,
         type: true,
         content: true,
         reportImages: true,
+        createdAt: true,
         reporter: {
           select: {
             id: true,
@@ -87,18 +100,29 @@ export class ReportService {
             avatar: true,
           }
         },
-      }
+      },
+      orderBy: { createdAt: 'desc' },
     });
+
+    const total = await this.prismaService.report.count({ where });
+
+    return {
+      reports,
+      total,
+      totalPages: Math.ceil(total / limit),
+      currentPage: page,
+    }
   }
 
   findOne(id: string) {
-    return this.prismaService.report.findUnique({
+    return this.prismaService.report.findUniqueOrThrow({
       where: { id },
       select: {
         id: true,
         type: true,
         content: true,
         reportImages: true,
+        createdAt: true,
         reporter: {
           select: {
             id: true,
@@ -122,5 +146,4 @@ export class ReportService {
       },
     });
   }
-
 }
