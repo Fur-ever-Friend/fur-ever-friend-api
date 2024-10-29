bb3f6aca98284c2682fbbd93617cb7aef943ca6b
EverydayImcalculating <87575444+EverydayImcalculating@users.noreply.github.com>
2024-10-29 16:53:57 +0700

    feat: Add initial database schema

---
+++ prisma/migrations/20241029094836_init/migration.sql
@@ -0,0 +1,352 @@
+-- CreateEnum
+CREATE TYPE "report_types" AS ENUM ('SERVICE_QUALITY', 'CUSTOMER_BEHAVIOR', 'UNPROFESSIONAL_CONDUCT', 'INAPPROPRIATE_COMMUNICATION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "roles" AS ENUM ('CUSTOMER', 'PETSITTER', 'ADMIN');
+
+-- CreateEnum
+CREATE TYPE "states" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- CreateEnum
+CREATE TYPE "account_states" AS ENUM ('ACTIVE', 'INACTIVE', 'BANNED');
+
+-- CreateEnum
+CREATE TYPE "payment_states" AS ENUM ('PENDING', 'SUCCEEDED', 'FAILED');
+
+-- CreateEnum
+CREATE TYPE "activity_states" AS ENUM ('PENDING', 'REJECTED', 'CANCELLED', 'ASSIGNED', 'IN_PROGRESS', 'RETURNING', 'FAILED', 'COMPLETED');
+
+-- CreateEnum
+CREATE TYPE "genders" AS ENUM ('MALE', 'FEMALE');
+
+-- CreateEnum
+CREATE TYPE "service_types" AS ENUM ('FEEDING', 'GROOMING', 'EXERCISING', 'TRAINING', 'ADMINISTERING_MEDICATION', 'RELAXATION');
+
+-- CreateTable
+CREATE TABLE "activities" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "detail" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "start_date_time" TIMESTAMP(3) NOT NULL,
+    "end_date_time" TIMESTAMP(3) NOT NULL,
+    "pickup_point" TEXT NOT NULL,
+    "state" "activity_states" NOT NULL DEFAULT 'PENDING',
+    "price" DOUBLE PRECISION,
+    "customer_id" TEXT NOT NULL,
+    "petsitter_id" TEXT,
+
+    CONSTRAINT "activities_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "activity_progresses" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "activity_id" TEXT NOT NULL,
+    "images" TEXT[],
+
+    CONSTRAINT "activity_progresses_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "petsitter_requests" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "state" "states" NOT NULL DEFAULT 'PENDING',
+    "price" DOUBLE PRECISION,
+    "message" TEXT,
+    "activity_id" TEXT NOT NULL,
+    "petsitter_id" TEXT NOT NULL,
+
+    CONSTRAINT "petsitter_requests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" BOOLEAN NOT NULL DEFAULT false,
+    "service_type" "service_types" NOT NULL,
+    "detail" TEXT NOT NULL,
+    "activity_service_id" TEXT NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "activity_services" (
+    "id" TEXT NOT NULL,
+    "pet_id" TEXT NOT NULL,
+    "activity_id" TEXT NOT NULL,
+
+    CONSTRAINT "activity_services_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "customers" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favourites" (
+    "id" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+    "petsitter_id" TEXT NOT NULL,
+
+    CONSTRAINT "favourites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "content" TEXT NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "activity_id" TEXT NOT NULL,
+    "petsitter_id" TEXT NOT NULL,
+    "customer_id" TEXT NOT NULL,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reports" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "type" "report_types" NOT NULL,
+    "content" TEXT NOT NULL,
+    "report_images" TEXT[],
+    "reporter_id" TEXT NOT NULL,
+    "reported_id" TEXT NOT NULL,
+    "activity_id" TEXT NOT NULL,
+
+    CONSTRAINT "reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(20) NOT NULL,
+    "age" INTEGER NOT NULL,
+    "weight" DOUBLE PRECISION NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "gender" "genders" NOT NULL,
+    "personality" TEXT,
+    "allergy" TEXT,
+    "other_detail" TEXT,
+    "animal_type_id" TEXT,
+    "breed_id" TEXT,
+    "owner_id" TEXT NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "animal_types" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
+
+    CONSTRAINT "animal_types_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "breeds" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(30) NOT NULL,
+    "animal_type_id" TEXT,
+
+    CONSTRAINT "breeds_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "petsitters" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "quote" TEXT NOT NULL DEFAULT '',
+    "location" TEXT NOT NULL DEFAULT '',
+    "about" TEXT NOT NULL DEFAULT '',
+    "experience" TEXT NOT NULL DEFAULT '',
+    "rating" DOUBLE PRECISION NOT NULL DEFAULT 4.0,
+    "certificate_url" TEXT NOT NULL,
+    "cover_images" TEXT[],
+    "service_tags" TEXT[],
+
+    CONSTRAINT "petsitters_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstname" VARCHAR(30),
+    "lastname" VARCHAR(30),
+    "avatar" TEXT,
+    "phone" VARCHAR(20),
+    "refresh_token" TEXT,
+    "role" "roles" NOT NULL,
+    "account_status" "account_states" NOT NULL DEFAULT 'ACTIVE',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "admins" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "admins_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "payments" (
+    "id" TEXT NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "state" "payment_states" NOT NULL DEFAULT 'PENDING',
+    "transaction_id" TEXT NOT NULL,
+    "activity_id" TEXT NOT NULL,
+
+    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "qualifications" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(50) NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstname" VARCHAR(30) NOT NULL,
+    "lastname" VARCHAR(30) NOT NULL,
+    "phone" VARCHAR(20) NOT NULL,
+    "state" "states" NOT NULL DEFAULT 'PENDING',
+    "certificate_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "qualifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "customers_user_id_key" ON "customers"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "favourites_customer_id_petsitter_id_key" ON "favourites"("customer_id", "petsitter_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "reviews_activity_id_key" ON "reviews"("activity_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "animal_types_name_key" ON "animal_types"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "breeds_name_key" ON "breeds"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "petsitters_user_id_key" ON "petsitters"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_refresh_token_key" ON "users"("refresh_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "admins_user_id_key" ON "admins"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_transaction_id_key" ON "payments"("transaction_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "payments_activity_id_key" ON "payments"("activity_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "qualifications_email_key" ON "qualifications"("email");
+
+-- AddForeignKey
+ALTER TABLE "activities" ADD CONSTRAINT "activities_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activities" ADD CONSTRAINT "activities_petsitter_id_fkey" FOREIGN KEY ("petsitter_id") REFERENCES "petsitters"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activity_progresses" ADD CONSTRAINT "activity_progresses_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "petsitter_requests" ADD CONSTRAINT "petsitter_requests_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "petsitter_requests" ADD CONSTRAINT "petsitter_requests_petsitter_id_fkey" FOREIGN KEY ("petsitter_id") REFERENCES "petsitters"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "tasks" ADD CONSTRAINT "tasks_activity_service_id_fkey" FOREIGN KEY ("activity_service_id") REFERENCES "activity_services"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activity_services" ADD CONSTRAINT "activity_services_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "activity_services" ADD CONSTRAINT "activity_services_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "customers" ADD CONSTRAINT "customers_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favourites" ADD CONSTRAINT "favourites_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favourites" ADD CONSTRAINT "favourites_petsitter_id_fkey" FOREIGN KEY ("petsitter_id") REFERENCES "petsitters"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_petsitter_id_fkey" FOREIGN KEY ("petsitter_id") REFERENCES "petsitters"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_reporter_id_fkey" FOREIGN KEY ("reporter_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_reported_id_fkey" FOREIGN KEY ("reported_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "reports" ADD CONSTRAINT "reports_activity_id_fkey" FOREIGN KEY ("activity_id") REFERENCES "activities"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_animal_type_id_fkey" FOREIGN KEY ("animal_type_id") REFERENCES "animal_types"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_breed_id_fkey" FOREIGN KEY ("breed_id") REFERENCES "breeds"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "breeds" ADD CONSTRAINT "breeds_animal_type_id_fkey" FOREIGN KEY ("animal_type_id") REFERENCES "animal_types"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "petsitters" ADD CONSTRAINT "petsitters_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "admins" ADD CONSTRAINT "admins_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
